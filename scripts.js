6a3e7fadedea28fbc2ba98a4ea906aabfe95795f
gavema <gavema_@hotmail.com>
2022-02-20 17:50:14 -0600

    se añade primera tarea

---
+++ scripts.js
@@ -0,0 +1,24 @@
+//Ejercicio 1
+/*Crea un programa para determinar el área de un triángulo*/
+/*
+let base=parseInt(prompt('Introduzca la medida de la base en cm'));
+let altura=parseInt(prompt('Introduce la medida de la altura en cm'));
+
+alert('El área del triángulo es: ' + ((base*altura)/2) + 'cm²');
+*/
+
+//Ejercicio 2
+/* Un vendedor que tiene su sueldo base, recibe además una comisión del 5% del total de sus ventas del mes.  El mes de Julio realizó 2 ventas, el quiere saber cuánto ganará el mes de Julio. */
+/*
+let sueldoBase=parseInt(prompt('Cuanto es tu sueldo base?'));
+let ventasExtras=parseInt(prompt('Ingresa el monto de tus ventas extras del mes:'));
+let sueldoTotal=parseInt(prompt('Tu sueldo total es: ' + ((ventasExtras*.05)+sueldoBase) +'pesos'));
+*/
+
+//Ejercicio 3
+/* Un empleado tuvo un aumento del 10% de su sueldo, necesita saber cuál será el total de su nuevo sueldo. */
+/*
+let sueldoActual=parseInt(prompt('Cual es tu sueldo actual?'));
+let nuevoSueldo=parseInt(prompt('Felicidades! Tu nuevo sueldo será: '+ ((sueldoActual*.10)+sueldoActual)+'pesos'));
+*/
+
